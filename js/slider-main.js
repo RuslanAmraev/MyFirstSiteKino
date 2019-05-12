70abfad15ea6064373970e9cdffea90b4ebc11b0
RuslanAmraev <46445130+RuslanAmraev@users.noreply.github.com>
2019-05-12 18:14:53 +0600

    adaptive fix

--- js/slider-main.js
+++ js/slider-main.js
@@ -1,48 +1,48 @@
-slideFilm()
-function slideFilm(){
-		setTimeout(function() {	
-		document.getElementById('main-1').style.display = 'block';
-		document.getElementById('main-2').style.display = 'none';
-		document.getElementById('main-3').style.display = 'none';
-		document.getElementById('main-4').style.display = 'none';
-		}, 5000);
-		setTimeout(function() {	
-	slideFilm2()
-	}, 5000);
-}
+// slideFilm()
+// function slideFilm(){
+// 		setTimeout(function() {	
+// 		document.getElementById('main-1').style.display = 'block';
+// 		document.getElementById('main-2').style.display = 'none';
+// 		document.getElementById('main-3').style.display = 'none';
+// 		document.getElementById('main-4').style.display = 'none';
+// 		}, 5000);
+// 		setTimeout(function() {	
+// 	slideFilm2()
+// 	}, 5000);
+// }
 
-function slideFilm2(){
-	setTimeout(function() {		
-		document.getElementById('main-1').style.display = 'none';
-		document.getElementById('main-2').style.display = 'block';
-		document.getElementById('main-3').style.display = 'none';
-		document.getElementById('main-4').style.display = 'none';
-	}, 5000);
-		setTimeout(function() {
-	slideFilm3();
-	}, 5000);
-}
+// function slideFilm2(){
+// 	setTimeout(function() {		
+// 		document.getElementById('main-1').style.display = 'none';
+// 		document.getElementById('main-2').style.display = 'block';
+// 		document.getElementById('main-3').style.display = 'none';
+// 		document.getElementById('main-4').style.display = 'none';
+// 	}, 5000);
+// 		setTimeout(function() {
+// 	slideFilm3();
+// 	}, 5000);
+// }
 
-function slideFilm3(){
-	setTimeout(function() {		
-		document.getElementById('main-1').style.display = 'none';
-		document.getElementById('main-2').style.display = 'none';
-		document.getElementById('main-3').style.display = 'block';
-		document.getElementById('main-4').style.display = 'none';
-	}, 5000);
-		setTimeout(function() {
-	slideFilm4();
-	}, 5000);
-}
+// function slideFilm3(){
+// 	setTimeout(function() {		
+// 		document.getElementById('main-1').style.display = 'none';
+// 		document.getElementById('main-2').style.display = 'none';
+// 		document.getElementById('main-3').style.display = 'block';
+// 		document.getElementById('main-4').style.display = 'none';
+// 	}, 5000);
+// 		setTimeout(function() {
+// 	slideFilm4();
+// 	}, 5000);
+// }
 
-function slideFilm4(){
-	setTimeout(function() {		
-		document.getElementById('main-1').style.display = 'none';
-		document.getElementById('main-2').style.display = 'none';
-		document.getElementById('main-3').style.display = 'none';
-		document.getElementById('main-4').style.display = 'block';
-	}, 5000);
-		setTimeout(function() {
-	slideFilm();
-	}, 5000);
-}
+// function slideFilm4(){
+// 	setTimeout(function() {		
+// 		document.getElementById('main-1').style.display = 'none';
+// 		document.getElementById('main-2').style.display = 'none';
+// 		document.getElementById('main-3').style.display = 'none';
+// 		document.getElementById('main-4').style.display = 'block';
+// 	}, 5000);
+// 		setTimeout(function() {
+// 	slideFilm();
+// 	}, 5000);
+// }
